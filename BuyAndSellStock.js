912cfc0b7e60d87cfa8e44ce9dc0a4b389f8f788
kaietSwin <105062270@student.swin.edu.au>
2025-04-15 11:25:25 +1000

    fix code

--- BuyAndSellStock.js
+++ BuyAndSellStock.js
@@ -1,22 +1,16 @@
-var maxProfit = function (prices) {
-  let min = prices[0];
-  let max = prices[0];
-  let profit = max - min;
-  for (price of prices) {
-    if (price < min) {
-      min = price;
-      max = 0;
-    }
-
-    if (price > max) {
-      max = price;
-    }
-    if (max - min > profit) {
-      profit = max - min;
-    }
+let min = prices[0];
+let max = prices[0];
+let profit = max - min;
+for (price of prices) {
+  if (price < min) {
+    min = price;
+    max = price;
+  } else if (price > max) {
+    max = price;
   }
+  if (max - min > profit) {
+    profit = max - min;
+  }
+}
 
-  if (profit < 0) return 0;
-
-  return profit;
-};
+return profit;
